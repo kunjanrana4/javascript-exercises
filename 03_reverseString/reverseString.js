58703f103034e0451295cbbe9f9389d62a4c1669
Kunjan <kunjanrana4@gmail.com>
2024-06-06 11:40:09 -0400

    Fix: no reversed string returned
    Add logic for reversing the string and returned the result.
    Updated README.md to mention all test cases passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    reverseArray = [];
+    for (let pos = word.length-1; pos >= 0; pos--) {
+        reverseArray.push(word[pos])
+    }
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
