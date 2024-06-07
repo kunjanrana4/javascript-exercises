c727d4ab24ff0a19c8f71f30dc71b4992ebd63a1
Kunjan <kunjanrana4@gmail.com>
2024-06-07 08:25:12 -0400

    Fix: return error for negative and non-number
    Added logic to return error if a number is negative or if it's not a number.
    If the number is not in order than the values will be reassigned and sum calculated.
    Updated the sum formula to reduce the time complexity so the result is displayed instantaneously instead of iterating over each number.
    Updated README.md to mention that all test cases passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,17 @@
 const sumAll = function(start, end) {
-    let sum = 0;
-    for (let i = start; i <= end; i++){
-        sum += i;
-    }
+    if (typeof(start) != "number" || typeof(end) != "number" || start < 0 || end < 0) return "ERROR";
+    if (start > end) [start, end] = [end, start];
+    // below code iterates over each number and adds the sum
+    // let sum = 0;
+    // for (let i = start; i <= end; i++){
+    //     sum += i;
+    // }
+
+    /* New formula solution
+       below code finds the sum of numbers of series x + (x+1) + (x+2) + ...... + (x+n)
+       It's a modified solution from formula 1 + 2 + 3 + ..... + n = (n * (n+1))/2
+       Here, the sum of 1...(x-1) is subtracted from the sum of 1...n to find the sum of all numbers from x...n */
+    let sum = (end * (end + 1)) / 2 - (start * (start - 1)) / 2;  
     return sum;
 };
 
