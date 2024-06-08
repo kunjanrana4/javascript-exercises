3e2249124631aec809b147ecb512137418cdd084
Kunjan <kunjanrana4@gmail.com>
2024-06-08 07:05:39 -0400

    Refactor: update leap year logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,18 @@
 const leapYears = function(year) {
-    if (year % 400 == 0) return true;
-    if (year % 100 == 0) return false;
-    if (year % 4 == 0 ) return true;
-    return false;
+    if (year % 4 == 0){
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 100 == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
